a550ac5b916d9961010305a2b59fc7e387d17d07
Bogdan <bogdan.voinalovych@gmail.com>
2024-08-19 23:59:09 +0300

    remove annoying comments

--- lua/plugins/git-signs.lua
+++ lua/plugins/git-signs.lua
@@ -1,9 +1,7 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		require("gitsigns").setup({
-			-- Your custom configuration here
-		})
-	end,
+  "lewis6991/gitsigns.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("gitsigns").setup({})
+  end,
 }
